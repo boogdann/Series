797fc3348c70c88325be6f34938ea8aa95dd16e0
bogdan <griduskobogdan@gmail.com>
2023-12-07 21:42:31 +0300

    add adaptability (without slider)

--- src/components/home/Trailer/trailer.jsx
+++ src/components/home/Trailer/trailer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import YouTube from "react-youtube";
 import s from './trailer.module.css';
 import {useTranslation} from "react-i18next";
+import {Card, CardContent} from "@mui/material";
 
 const Trailer = (props) => {
     const {t} = useTranslation();
@@ -10,9 +11,10 @@ const Trailer = (props) => {
         <div className={s.trailer__container}>
             <h2 className={s.trailer__title}>{t('trailer.title')}</h2>
             <div className={s.trailer__block}>
-                <YouTube className={s.trailer__youtube} videoId={props.trailerID}/>
+                <CardContent className={s.trailer__content}>
+                    <YouTube className={s.trailer__youtube} videoId={props.trailerID}/>
+                </CardContent>
             </div>
-
         </div>
     );
 };
