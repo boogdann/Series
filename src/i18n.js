7d05d4b15bcb9547b969e40afcbebaafececb533
boogdann <griduskobogdan@gmail.com>
2023-12-13 01:01:28 +0300

    fix path to locales

--- src/i18n.js
+++ src/i18n.js
@@ -1,18 +1,21 @@
 import i18n from 'i18next'
 import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
-import { initReactI18next } from 'react-i18next'
+import {initReactI18next} from 'react-i18next'
 
 i18n.use(Backend)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-    fallbackLng: 'ru',
-    debug: true,
-    interpolation: {
-        escapeValue: false
-    },
-    returnEmptyString: false
-})
+        fallbackLng: 'ru',
+        debug: true,
+        interpolation: {
+            escapeValue: false
+        },
+        returnEmptyString: false,
+        backend: {
+            loadPath: 'http://boogdann.github.io/Series//locales/{{lng}}/translation.json'
+        }
+    })
 
 export default i18n
